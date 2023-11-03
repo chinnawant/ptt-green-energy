c54f35e9bd27b29072df4dbb2cf2b9531ff73939
chinnawat <zchinnawat.p@pttdigital.com>
2023-11-04 05:38:32 +0700

    test: scan

--- server/main.js
+++ server/main.js
@@ -2,14 +2,36 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config();
 
+app.disable("x-powered-by");
 
 
-// add middlewares
-app.use(cors({
-  origin: process.env.WHITELIST,
-}));
+const whitelist = process.env.WHITELISTED_DOMAINS.split(",")
+  .filter(domain => {
+    try {
+      new url.URL(domain);
+      return true;
+    } catch (_) {
+      console.warn(`Invalid domain in whitelist: ${domain}`);
+      return false;
+    }
+  });
+
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
