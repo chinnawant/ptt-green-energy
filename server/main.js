3ffa03419e0745642672a938108ca44b7d92a44f
chinnawat <zchinnawat.p@pttdigital.com>
2023-11-04 05:38:32 +0700

    SC: Scan report blackduck

--- server/main.js
+++ server/main.js
@@ -2,16 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config();
 
+app.disable("x-powered-by");
+
+const envWhitelist = process.env.WHITELISTED_DOMAINS || "";
+const whitelist = envWhitelist.split(",")
+  .filter(domain => {
+    try {
+      console.log("dd",domain)
+      new url.URL(domain);
+      return true;
+    } catch (_) {
+      console.warn(`Invalid domain in whitelist: ${domain}`);
+      return false;
+    }
+  });
 
 
-// add middlewares
-app.use(cors({
-  origin: process.env.WHITELIST,
-}));
-app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+app.use(express.static(path.join(__dirname, "..", "build")),cors(corsOptions));
+app.use(express.static("public"),cors(corsOptions),cors(corsOptions));
 
 
 // start express server on port 5000
