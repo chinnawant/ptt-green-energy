05bf1f42dd7475cd6333db7869cbaa0ddbc1ba49
Panyapron Wiratchadit <panyapron.w@nellika.co.th>
2023-12-01 10:44:41 +0700

    test sonar

--- jest.config.ts
+++ jest.config.ts
@@ -4,45 +4,52 @@
  */
 
 export default {
-    collectCoverage: true,
-    coverageDirectory: "./TestResults",
-    clearMocks: true,
-    reporters: [
-        "default",
-        "jest-junit",
-        [
-            "jest-silent-reporter",
-            {
-                useDots: true,
-            },
-        ],
-        [
-            "jest-html-reporters",
-            {
-                publicPath: "./TestResults/html-report",
-                filename: "report.html",
-                openReport: true,
-            },
-        ],
+  collectCoverage: true,
+  coverageDirectory: "./TestResults",
+  clearMocks: true,
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        suiteName: "jest tests",
+        outputDirectory: "./TestResults/Junit",
+        outputName: "junit.xml",
+      },
     ],
-    coveragePathIgnorePatterns: ["<rootDir>/src/apis"],
-    coverageReporters: ["lcov", "text"],
-   // coverageDirectory: "test-coverage",
-    coverageThreshold: {
-        global: {
-            branches: 30,
-            functions: 30,
-            lines: 30,
-            statements: 30,
-        },
+    [
+      "jest-silent-reporter",
+      {
+        useDots: true,
+      },
+    ],
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./TestResults/html-report",
+        filename: "report.html",
+        openReport: true,
+      },
+    ],
+  ],
+  coveragePathIgnorePatterns: ["<rootDir>/src/apis"],
+  coverageReporters: ["lcov", "text"],
+  // coverageDirectory: "test-coverage",
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
     },
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    maxWorkers: '50%',
-    moduleNameMapper: {
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-        "\\.(svg|pdf|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__test__/mocks/file-transform.ts"
-
-},
-}
+  },
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  maxWorkers: "50%",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(svg|pdf|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__test__/mocks/file-transform.ts",
+  },
+};
