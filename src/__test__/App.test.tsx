e547b23d40c31ceae57684f88724a125943fc57c
chinnawat <zchinnawat.p@pttdigital.com>
2023-12-28 15:55:12 +0700

    Update SonarQube exclusions

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import UserEvent from "@testing-library/user-event";
 import App from '../App';
+import ReactDOM from 'react-dom';
 
 describe('App component', () => {
   test('renders App component without crashing', async () => {
     render(<App />);
     expect(screen.getByText('PTT Green Energy Pte. Ltd.')).toBeInTheDocument();;
   });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  
 });
